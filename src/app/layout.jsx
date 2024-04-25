457346bb4c8c8228c826656f1e786ac06573753d
RamSekuy <ramaalim34@gmail.com>
2024-04-25 13:44:53 +0700

    deploy

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "./_components/navbarComponents/navbar";
+// import Navbar from "./_components/navbarComponents/navbar";
 import Footer from "./_components/footerComponents/footer";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className+" bg-gradient-to-tr from-blue-200 to-purple-200"}>
-        <Navbar/>
+        {/* <Navbar/> */}
         {children}
         <Footer/>
         </body>
